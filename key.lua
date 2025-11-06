00bbd0e06794a1c26027159e9e28b1449bd29f0d
MatthewGrarrick <nguyenbaohoang123456t@gmail.com>
2025-11-06 12:37:53 +0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,2 +1,20 @@
-hoang4567
-hoang1234
+-- 🔑 LOOPSC HUB - Key System 24h (Auto Expire)
+-- Cấu trúc: mỗi dòng 1 key|timestamp_hết_hạn
+
+local keys = [[
+LOOPSC-20251106-445204|1762493857
+]]
+
+local now = os.time()
+local validKeys = {}
+
+for line in keys:gmatch("[^\r\n]+") do
+    local key, expiry = line:match("(.+)|(%d+)")
+    if key and expiry then
+        if now <= tonumber(expiry) then
+            table.insert(validKeys, key)
+        end
+    end
+end
+
+return validKeys
